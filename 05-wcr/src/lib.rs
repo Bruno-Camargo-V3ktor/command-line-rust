ce7ce66c6e2cb027c2de057636ae56029b22f8f6
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-08-26 23:27:59 -0300

    chapter05: add Args for command

---
+++ 05-wcr/src/lib.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(
+    author = "Bruno-Camargo-Ferreira <brunp.ferreira.v3ktor@gmail.com>",
+    version,
+    about = "Rust version of 'wc'"
+)]
+pub struct Args {
+    ///Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+
+    ///Show line count
+    #[arg(short, long)]
+    lines: bool,
+
+    ///Show word count
+    #[arg(short, long)]
+    words: bool,
+
+    ///Show byte count
+    #[arg(short('c'), long)]
+    bytes: bool,
+
+    ///Show character count
+    #[arg(short('m'), long, conflicts_with("bytes"))]
+    chars: bool,
+}
